28697753eedca1160d51be2d9d57c75fd47e5ed2
Jan Jaap Siewers <janjaap@siewers.org>
2023-12-15 11:35:41 +0100

    mooier maken

--- index.php
+++ index.php
@@ -7,4 +7,36 @@ $con = new PDO($dsn, 'root', '');
 $stmt = $con->prepare("select doc_naam, telefoon, straat from docent where doc_naam = '$docentnaam'");
 $stmt->execute();
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-print_r($result);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table, td, tr {
+            border-collapse: collapse;
+            border: 1px solid blue;
+        }
+
+        td {
+            padding: 5px;
+        }
+    </style>
+</head>
+<body>
+    <table>
+<?php foreach($result as $row) { ?>
+    <tr>
+        <td><?= $row['doc_naam'] ?></td>
+        <td><?= $row['telefoon'] ?></td>
+        <td><?= $row['straat'] ?></td>
+    </tr>
+<?php  }  ?>
+    </table>
+</body>
+</html>
+
+
